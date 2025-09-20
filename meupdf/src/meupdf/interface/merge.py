00cc6ca733fcfa80de74c2b83fd75a43852d8509
Leandro Arndt <poetalirico@gmail.com>
2025-09-20 20:56:39 -0300

    Reausable components and styles

--- meupdf/src/meupdf/interface/merge.py
+++ meupdf/src/meupdf/interface/merge.py
@@ -1,12 +1,14 @@
+from typing import Literal
 import toga, asyncio
 
 from pathlib import Path
 import toga.constants
 from toga.style import pack
 
-from meupdf.interface.document_organizer import FileRow
+from meupdf.interface.common import FileRow, PageImage
 from meupdf.documents import pdf
-            
+from meupdf.interface.styles import flex_column_center_margin, flex_column_right, right_align, margin
+
 class MergeWindow(toga.Window):
     # documents:list[FileRow]
     add_button:toga.Button
@@ -17,21 +19,19 @@ class MergeWindow(toga.Window):
     merge_button:toga.Button
 
     def __init__(self, *args, **kwargs):
-        super().__init__(self, on_close=self.prepare_to_close, *args, **kwargs)
+        super().__init__(on_close=self.prepare_to_close, *args, **kwargs)
 
-        flex_column_right = pack.Pack(flex=1, direction=toga.constants.COLUMN, align_items=toga.constants.END)
-        right_align = pack.Pack(flex=0, direction=toga.constants.ROW, align_items=toga.constants.END)
         self.content = toga.Box(style=flex_column_right)
         self.add_button = toga.Button(_('Add document'), enabled=False, on_press=self.open_dialog)
         self.cancel_button = toga.Button(_('Cancel'), enabled=False, on_press=self.do_close)
-        self.merge_button = toga.Button(_('Merge'), enabled=False, on_press=self.save_file_dialog)
+        self.merge_button = toga.Button(text=_('Merge'), enabled=False, on_press=self.show_save_file_dialog)
         top_button_row = toga.Box(style=right_align)
         bottom_button_row = toga.Box(style=right_align)
-        self.document_organizer = toga.Box(style=pack.Pack(flex=1, direction=pack.COLUMN, margin=5))
+        self.document_organizer = toga.Box(style=flex_column_center_margin)#pack.Pack(flex=1, direction=pack.COLUMN, margin=5)) # type: ignore
         self.scroll = toga.ScrollContainer(vertical=True, horizontal=True, content=self.document_organizer, style=pack.Pack(flex=1))
         top_button_row.add(self.add_button)
         bottom_button_row.add(self.cancel_button, self.merge_button)
-        self.content.add(top_button_row, self.scroll, bottom_button_row)
+        self.content.add(top_button_row, self.scroll, bottom_button_row) # type: ignore
 
     def open_dialog(self, widget, first_selection=False):
         dialog = toga.OpenFileDialog(_('Select PDF files to merge'), file_types=['PDF'], multiple_select=True)
@@ -57,7 +57,7 @@ class MergeWindow(toga.Window):
         self.document_organizer.children[0].inhibit_buttons()
         self.document_organizer.children[-1].inhibit_buttons()
 
-    def save_file_dialog(self, widget):
+    def show_save_file_dialog(self, widget):
         dialog = toga.SaveFileDialog(
             _('Choose destination file'),
             f'{self.document_organizer.children[0].document.file_path.stem} - {_("merged")}.{pdf.DOCUMENT_FORMAT}',
@@ -71,7 +71,7 @@ class MergeWindow(toga.Window):
         if file:
             self.merge(file)
 
-    def prepare_to_close(self, window, **kwargs):
+    def prepare_to_close(self, window, **kwargs) -> Literal[True]:
         for row in self.document_organizer.children:
             row.document.close()
         return True
